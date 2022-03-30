9069aa7c0a45704280dace9248eb51b530bec81c
Alex Blanke <alexblanke98@gmail.com>
2022-03-30 13:21:58 -0500

    Made a prompt for user input and function to confirm choice

--- script.js
+++ script.js
@@ -1,51 +1,84 @@
 
-/* function to play the game
+// function to play the game
 
-    all in while loop
-        window.confirm to keep playing or stop
-
-
-Some kind of input from user (RPS)
-                                            window.prompt()
+//     all in while loop
+//         window.confirm to keep playing or stop
 
 
-
-Some kind of input from computer ()
-
-Some function to randomly pick from RPS
-                                            function getRandomInt(max) {
-                                                return Math.floor(Math.random() * max);
-                                            }
-
-                                            console.log(getRandomInt(3));
+// Some kind of input from user (RPS)
+//                                             window.prompt()
+var userInput = prompt("Rock, Paper, or Scissors?");
 
 
+var userFunction = function(entry) {
+  if (entry.toLowerCase() == "rock") {
+    console.log("You put rock");
+      // 0
+    return "rock";
+  }
 
+  else if (entry.toLowerCase() == "paper") {
+    console.log("You put paper");
+    // 1
+    return "paper";
+  }
 
-Something to compare the two selections
+  else if (entry.toLowerCase() == "scissors") {
+    console.log("You put scissors");
+    
+    // 2
+    return "scissors";
+  }
 
-Choose a winner
+  else {
+    entry = prompt("Please type either rock, paper, or scissors");
+  }
+}
 
+window.alert(userFunction(userInput));
 
-
-Display outcome
-        window.alert()
-
-
-
-Log results
-        use an array or multiple vars
-
-Display results
-        window.alert()
-
-Ask to play again or quit
-
+  
+// there are many ways to use the prompt feature
 
 
 
 
-*/
+// Some kind of input from computer ()
+
+// Some function to randomly pick from RPS
+//                                             function getRandomInt(max) {
+//                                                 return Math.floor(Math.random() * max);
+//                                             }
+
+//                                             console.log(getRandomInt(3));
+
+
+
+
+// Something to compare the two selections
+
+// Choose a winner
+
+
+
+// Display outcome
+//         window.alert()
+
+
+
+// Log results
+//         use an array or multiple vars
+
+// Display results
+//         window.alert()
+
+// Ask to play again or quit
+
+
+
+
+
+//
 
 
 
