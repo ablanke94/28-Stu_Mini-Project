79a9bb7234ec24df919282c7540fbb949a565730
Alex Blanke <alexblanke98@gmail.com>
2022-03-30 12:02:03 -0500

    commented out a plan in script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+
+/* function to play the game
+
+    all in while loop
+        window.confirm to keep playing or stop
+
+
+Some kind of input from user (RPS)
+                                            window.prompt()
+
+
+
+Some kind of input from computer ()
+
+Some function to randomly pick from RPS
+                                            function getRandomInt(max) {
+                                                return Math.floor(Math.random() * max);
+                                            }
+
+                                            console.log(getRandomInt(3));
+
+
+
+
+Something to compare the two selections
+
+Choose a winner
+
+
+
+Display outcome
+        window.alert()
+
+
+
+Log results
+        use an array or multiple vars
+
+Display results
+        window.alert()
+
+Ask to play again or quit
+
+
+
+
+
+*/
+
+
+
+
+// 
+
+
+
+
+
